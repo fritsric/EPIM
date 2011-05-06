1a8d2bb985f787a6dccad5d5a5eedc83d711d092
Prcek <prcek@prcek-laptop.(none)>
2011-05-06 16:36:18 +0200

    Přidána do ostrého projektu pro generování + převod Docx->Pdf

---
+++ IndepModExporter/src/cz/cvut/hnatuluk/factories/PDFFactory/PdfDocument.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.cvut.hnatuluk.factories.PDFFactory;
+
+import cz.cvut.hnatuluk.factories.WordFactory.WordDocument;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.wml.Body;
+import org.docx4j.wml.Ftr;
+import org.docx4j.wml.Hdr;
+import org.docx4j.wml.ObjectFactory;
+
+/**
+ *
+ * @author prcek
+ */
+public class PdfDocument extends WordDocument{
+
+    public PdfDocument(ObjectFactory createFactory, Body documentBody, WordprocessingMLPackage wmlPackage,Hdr h, Ftr f) {
+        super(createFactory, documentBody, wmlPackage, h, f);
+    }
+    
+    public static PdfDocument convertDocxToPdf(WordDocument doc){
+        return new PdfDocument(doc.getCreateFactory(), doc.getDocumentBody(), doc.getWmlPackage(), doc.getHeader(), doc.getFooter());
+    }
+}
